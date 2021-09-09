dfe8d130b5679bfacc3c1e152648f4dc6bdc16d7
AnDrEyScode <71716659+AnDrEyScode@users.noreply.github.com>
2021-09-09 21:04:13 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(
+            function (){
+              $('.first-button-menu').click(function (){
+                        $('.navbar').toggleClass('hidden');
+                        $('.first-button-menu-line').toggleClass('orange');
+                      }
+
+                )
+            }
+    );
