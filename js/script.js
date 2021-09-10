b6341c15f772492fd79bdbe166489f8654410824
AnDrEyScode <71716659+AnDrEyScode@users.noreply.github.com>
2021-09-10 20:57:00 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+$('.ninth-answer').hide();
+$('.line-answer').hide();
+$('.eighth-price-container').hide();
+
+
 $(document).ready(
             function (){
               $('.first-button-menu').click(function (){
@@ -5,6 +10,31 @@ $(document).ready(
                         $('.first-button-menu-line').toggleClass('orange');
                       }
 
+                )
+            }
+    );
+
+    
+
+        $(document).ready(
+            function (){
+              $('div.plus').click(function (){
+                        $(this).next().next().toggle(500);
+                        $(this).next().toggle(500);
+                        $(this).toggleClass('plus-rotate');
+                        $(this).children('div').toggleClass('plus-blue').animate();
+                      }
+                )
+            }
+    );
+
+
+    $(document).ready(
+            function (){
+              
+              $('.eighth-action-show').click(function (){
+                        $(this).next().toggle(500);
+                      }
                 )
             }
     );
